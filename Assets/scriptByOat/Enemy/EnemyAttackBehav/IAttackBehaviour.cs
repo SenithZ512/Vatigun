d3c7e9f403ccd35c4ccc81b1a3d594d3571f7980
SenithZ512 <chakza1234@gmail.com>
2026-04-18 20:21:41 +0700

    Revert "Update BackUp"
    This reverts commit 3900797e60f51626bc39f2cb0b534731019c611f.

--- Assets/scriptByOat/Enemy/EnemyAttackBehav/IAttackBehaviour.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IAttackBehaviour 
-{
-    void Attack(EnemyStateManager state);
-}
--- Assets/scriptByOat/Interact/AmmoTypeSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "New Ammo Type", menuName = "Shooter/Ammo Type")]
-public class AmmoTypeSO : ScriptableObject
-{
-    public string ammoTypeName;
-}
-
